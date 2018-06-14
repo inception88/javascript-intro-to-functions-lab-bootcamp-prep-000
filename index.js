70f75fc25dfd593181a7d98d74858dd71e306b16
Benjamin Jones <bljones36@gmail.com>
2018-06-14 23:13:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,15 +9,3 @@ function whisper(string) {
 function logShout(string) {
   console.log(string.toUpperCase)
 }
-
-describe('logShout(string)', function() {
-  it('calls console.log() its one argument in all caps', function() {
-    const spy = expect.spyOn(console, 'log').andCallThrough()
-
-    logShout('hello')
-
-    expect(spy).toHaveBeenCalledWith('HELLO')
-
-    console.log.restore()
-  })
-})
